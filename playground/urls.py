8d588c06c9a853e4a87988f72b7a2443a67bc980
Ghua8088 <137153710+Ghua8088@users.noreply.github.com>
2025-04-13 22:56:22 +0530

    main url verification

--- playground/urls.py
+++ playground/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('hello/', views.say_hello, name='say_hello'),
-    path('visualize/pathfind/', views.pathfind, name='pathfind'),
+    path('', views.pathfind, name='pathfind'),
     path('get_current_time/',views.get_current_time,name='get_current_time')
 ]
